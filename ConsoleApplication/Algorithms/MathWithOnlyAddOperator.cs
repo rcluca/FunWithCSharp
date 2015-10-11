c25f9513a648c6396b582ee827309e34d75cbe71
rcluca <rc_luca@yahoo.com>
2015-10-11 13:22:20 -0400

    Implemented Add/Subtract/Multiply/Divide operations using only add operator.

---
+++ ConsoleApplication/Algorithms/MathWithOnlyAddOperator.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Algorithms
+{
+    public static class MathWithOnlyAddOperator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            b = (-1 ^ b) + 1;
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            bool isNegative = false;
+
+            if (a < 0 && b < 0)
+            {
+                a = (-1 ^ a) + 1;
+                b = (-1 ^ b) + 1;
+            }
+            else if (a < 0)
+            {
+                isNegative = true;
+                a = (-1 ^ a) + 1;
+            }
+            else if (b < 0)
+            {
+                isNegative = true;
+                b = (-1 ^ b) + 1;
+            }
+
+            int total = 0;
+            for (int i = 0; i < b; i++)
+                total += a;
+
+            if (isNegative)
+                return (-1 ^ total) + 1;
+            else
+                return total;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+
+            if (a == 0)
+                return 0;
+
+            bool isNegative = false;
+
+            if (a < 0 && b < 0)
+            {
+                a = (-1 ^ a) + 1;
+                b = (-1 ^ b) + 1;
+            }
+            else if (a < 0)
+            {
+                isNegative = true;
+                a = (-1 ^ a) + 1;
+            }
+            else if (b < 0)
+            {
+                isNegative = true;
+                b = (-1 ^ b) + 1;
+            }
+
+            int total = b;
+
+            for (int i = 1; i <= a; i++)
+            {
+                if (total == a)
+                {
+                    if (isNegative)
+                        return (-1 ^ i) + 1;
+                    else
+                        return i;
+                }
+                else if (total > a)
+                {
+                    if (isNegative)
+                        return (-1 ^ (i - 1)) + 1;
+                    else
+                        return i - 1;
+                }
+                else
+                    total += b;
+            }
+
+            // Required because compiler thinks "Not all code paths return a value"
+            throw new Exception("Could not find answer.");
+        }
+    }
+}
---
+++ Test/MathWithOnlyAddOperatorTests.cs
@@ -0,0 +1,508 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication.Algorithms;
+
+namespace Test
+{
+    [TestClass]
+    public class MathWithOnlyAddOperatorTests
+    {
+        #region Subtract
+
+        [TestMethod]
+        public void SubtractPositiveLargerPositiveSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(10, 5);
+
+            // Assert
+            Assert.AreEqual(5, answer);
+        }
+
+        [TestMethod]
+        public void SubtractPositiveSmallerPositiveLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(7, 11);
+
+            // Assert
+            Assert.AreEqual(-4, answer);
+        }
+
+        [TestMethod]
+        public void SubtractPositiveEqualPositiveEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(12, 12);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void SubtractLargerZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(9, 0);
+
+            // Assert
+            Assert.AreEqual(9, answer);
+        }
+
+        [TestMethod]
+        public void SubtractZeroLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(0, 6);
+
+            // Assert
+            Assert.AreEqual(-6, answer);
+        }
+
+        [TestMethod]
+        public void SubtractNegativeLargerNegativeSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(-10, -5);
+
+            // Assert
+            Assert.AreEqual(-5, answer);
+        }
+
+        [TestMethod]
+        public void SubtractNegativeSmallerNegativeLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(-7, -11);
+
+            // Assert
+            Assert.AreEqual(4, answer);
+        }
+
+        [TestMethod]
+        public void SubtractNegativeEqualNegativeEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(-12, -12);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void SubtractSmallerZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(-9, 0);
+
+            // Assert
+            Assert.AreEqual(-9, answer);
+        }
+
+        [TestMethod]
+        public void SubtractZeroSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(0, -6);
+
+            // Assert
+            Assert.AreEqual(6, answer);
+        }
+
+        [TestMethod]
+        public void SubtractPositiveNegative()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(4, -5);
+
+            // Assert
+            Assert.AreEqual(9, answer);
+        }
+
+        [TestMethod]
+        public void SubtractNegativePositive()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(-7, 8);
+
+            // Assert
+            Assert.AreEqual(-15, answer);
+        }
+
+        [TestMethod]
+        public void SubtractZeroZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Subtract(0, 0);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        #endregion
+
+        #region Multiply
+
+        [TestMethod]
+        public void MultiplyPositiveLargerPositiveSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(10, 5);
+
+            // Assert
+            Assert.AreEqual(50, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyPositiveSmallerPositiveLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(7, 11);
+
+            // Assert
+            Assert.AreEqual(77, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyPositiveEqualPositiveEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(12, 12);
+
+            // Assert
+            Assert.AreEqual(144, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyLargerZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(9, 0);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyZeroLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(0, 6);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyNegativeLargerNegativeSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(-10, -5);
+
+            // Assert
+            Assert.AreEqual(50, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyNegativeSmallerNegativeLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(-7, -11);
+
+            // Assert
+            Assert.AreEqual(77, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyNegativeEqualNegativeEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(-12, -12);
+
+            // Assert
+            Assert.AreEqual(144, answer);
+        }
+
+        [TestMethod]
+        public void MultiplySmallerZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(-9, 0);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyZeroSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(0, -6);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyPositiveNegative()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(4, -5);
+
+            // Assert
+            Assert.AreEqual(-20, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyNegativePositive()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(-7, 8);
+
+            // Assert
+            Assert.AreEqual(-56, answer);
+        }
+
+        [TestMethod]
+        public void MultiplyZeroZero()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Multiply(0, 0);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        #endregion
+
+        #region Divide
+
+        [TestMethod]
+        public void DividePositiveLargerPositiveSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(10, 5);
+
+            // Assert
+            Assert.AreEqual(2, answer);
+        }
+
+        [TestMethod]
+        public void DividePositiveSmallerPositiveLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(7, 11);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DividePositiveEqualPositiveEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(12, 12);
+
+            // Assert
+            Assert.AreEqual(1, answer);
+        }
+
+        [TestMethod]
+        public void DivideLargerZero()
+        {
+            // Arrange
+
+            // Act
+            try
+            {
+                int answer = MathWithOnlyAddOperator.Divide(9, 0);
+            }
+
+            // Assert
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(DivideByZeroException));
+            }
+        }
+
+        [TestMethod]
+        public void DivideZeroLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(0, 6);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DivideNegativeLargerNegativeSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(-10, -5);
+
+            // Assert
+            Assert.AreEqual(2, answer);
+        }
+
+        [TestMethod]
+        public void DivideNegativeSmallerNegativeLarger()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(-7, -11);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DivideNegativeEqualNegativeEqual()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(-12, -12);
+
+            // Assert
+            Assert.AreEqual(1, answer);
+        }
+
+        [TestMethod]
+        public void DivideSmallerZero()
+        {
+            // Arrange
+
+            // Act
+            try
+            {
+                int answer = MathWithOnlyAddOperator.Divide(-9, 0);
+            }
+
+            // Assert
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(DivideByZeroException));
+            }
+        }
+
+        [TestMethod]
+        public void DivideZeroSmaller()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(0, -6);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DividePositiveNegative()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(4, -5);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DivideNegativePositive()
+        {
+            // Arrange
+
+            // Act
+            int answer = MathWithOnlyAddOperator.Divide(-7, 8);
+
+            // Assert
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestMethod]
+        public void DivideZeroZero()
+        {
+            // Arrange
+
+            // Act
+            try
+            {
+                int answer = MathWithOnlyAddOperator.Divide(0, 0);
+            }
+
+            // Assert
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(DivideByZeroException));
+            }
+        }
+
+        #endregion
+    }
+}
